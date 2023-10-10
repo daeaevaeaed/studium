7ec816503f90ad2851bf1427025bed029bdea78a
daeaevaeaed <fischertechniklol@yahoo.com>
2023-10-10 18:35:59 +0200

    neuer digitech ordner

--- Info_Vorkurs/pre_scripting.py
+++ Info_Vorkurs/pre_scripting.py
@@ -1,30 +1,3 @@
 
 
 
-
-def checker(inputString):
-    chronology = []
-    opening = ["{","[","("]
-    closing = ["}","]",")"]
-    for i in inputString:
-        if i in closing or i in opening:
-            if i in opening:
-                chronology.append(i)
-
-
-            if i in closing and chronology:
-
-                if opening[closing.index(i)] == chronology[-1]:
-                    del chronology[-1]
-                else:
-                    return False
-
-    if chronology:
-        return False
-    return True
-
-
-print(checker(input("was willst du testen ?")))
-
-
-
