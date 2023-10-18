81b14bc1f4cf71c59799a62263e42963e59ec029
daeaevaeaed <fischertechniklol@yahoo.com>
2023-10-18 19:16:48 +0200

    w

--- LinAlg/numpy zeugs.py
+++ LinAlg/numpy zeugs.py
@@ -1,8 +1,13 @@
 import numpy as np
 # Parameter :
-A=np.array([[1,3,-1],[4,-2,8]])
-B=np.array([[-3,9,3],[-6,6,3]])
+A=np.array([[4,-3,2],[6,2,5],[-1,-2,3]])
+B=np.array([[3,4],[1,2],[5,6]])
+U=np.array([[0],[2],[-4]])
+V=np.array([[1],[3],[-3]])
 # print(A+B)
 # print(-2*A)
 # print(B/3)
-print(B*A)
+# print(V.T@B)
+# print(np.linalg.solve(np.array([[2 , -1] ,[ -3 ,2]]),np. array ([1 ,0]) ))
+
+print(A,"awdw", np.linalg.inv(A))
