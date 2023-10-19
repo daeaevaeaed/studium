e6cbb7a07e9f48af27978bffaafa76d9c916bc96
daeaevaeaed <fischertechniklol@yahoo.com>
2023-10-19 13:08:34 +0200

    zsuammenfassung der Numpy befehle

--- LinAlg/numpy zeugs.py
+++ LinAlg/numpy zeugs.py
@@ -9,5 +9,27 @@ V=np.array([[1],[3],[-3]])
 # print(B/3)
 # print(V.T@B)
 # print(np.linalg.solve(np.array([[2 , -1] ,[ -3 ,2]]),np. array ([1 ,0]) ))
+M=np.diag([1,2,3])
+print(M)
 
-print(A,"awdw", np.linalg.inv(A))
+
+"""
+vektoren
+np.dot(v,w): skalarprodukt von v und w (v1*w1 + v2*w1...)
+np.linalg.norm(v): länge des Vektors V
+np.cross(v,w): kreuzprodukt von V und W
+
+--------------
+
+np.array[ [zeile 1], [zeile 2]... [zeile n]]
+
+np.linalg.inv(Matrix): produziert die inverse einer Matrix
+M=A.T: Matrix M = transposition von Matrix A
+M = A@B: Matrix M = Matrix A _matrixprodukt_ Matrix B
+np.zeros(n): n x n matrix mit nur nullen
+np.eye(x,y): x spalten y zeilen mit nur nullen außer diagonale, da nur einser
+np.linalg.inv(A) inverse von Matrix A
+np.diag([a,b,c...]): Matrix mit zahlen a b c ... auf der Diagonalen
+np.trace(A): Summe der Spur (Summe der Zahlen auf der Matrix diagonalen)
+np.linalg.det(A) determinante der Matrix A
+"""
