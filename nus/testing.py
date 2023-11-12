d7b0aae86a47ccc8084f3bf40112ff5dd854274f
daeaevaeaed <fischertechniklol@yahoo.com>
2023-11-13 00:28:16 +0100

    argh

--- nus/testing.py
+++ nus/testing.py
@@ -5,7 +5,7 @@ width = 10
 position = 0 # 1 2 3 4
 breite = 5
 for timer in range(50):
-    zahl =  (-1)**(math.ceil(timer/breite))
+    zahl =  (-1)**(math.floor(timer/breite))
     print("timer", timer, "zahl", zahl)
 # breite = 0
 # x = 0
